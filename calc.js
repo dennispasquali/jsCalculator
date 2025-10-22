10612891a1514281abaa513a4c61210f86f300c9
dennis pasquali <dennispasquali206@gmail.com>
2025-10-22 16:48:05 +0200

    inizializzazione js e html

---
+++ calc.js
@@ -0,0 +1,33 @@
+let result=0;
+
+
+//array button html
+let allBtn=document.querySelectorAll('button');
+
+allBtn.array.forEach(btn => {
+    console.log(btn);
+    btn.addEventListener('click', (e) =>  {
+        
+    });
+});
+function somma(a,b) {
+    return a+b;
+}
+
+function divisione(a,b) {
+    return a/b;
+}
+
+function moltiplicazione(a,b) {
+    return a*b;
+}
+
+function sottrazione(a,b) {
+    return a-b;
+}
+
+
+document.addEventListener('onclick', e => {
+
+})
+
